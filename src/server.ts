cef4d5c52bff33bd72c22d804b21b1db102294ba
Italo Fernandes <italo.avelino@outlook.com.br>
2024-05-04 00:17:22 -0300

    feat: initial template

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import * as dotenv from "dotenv"
+
+dotenv.config({ path: '.env'})
+
+import { router } from './routes';
+
+const server = express();
+
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: false}));
+server.use('/api', router);
+
+export { server };
